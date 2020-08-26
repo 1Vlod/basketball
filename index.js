ec73da5ee299b7474f841401c259a7306cabd1c2
Vlad <example@gmail.comg>
2020-08-26 17:08:11 +0300

    start rebuilding game procces

--- index.js
+++ index.js
@@ -5,63 +5,160 @@ const $score = document.querySelector("#score")
 const $btn_play = document.querySelector("#btn")
 const $messageWin = document.querySelector("#messageWin")
 const $messageAct = document.querySelector("#messageAct")
-
+const $timer = document.querySelector("#timer")
 
 //initialization
 let leftSideScore;
 let rightSideScore;
 let ballKeep = null;
-let leftSideThrowChances = 99;
-let rightSideThrowChances = 99;
-let leftSideChances = 50;
-let rightSideChances = 50;
-let timeLeft = 15;
+const chances = {
+  throwChances: {
+    left: 60,
+    right: 70
+  },
+  stealChances: {
+    left: 50,
+    right: 50
+  },
+  faceOffChances: {
+    left: 50,
+    right: 50
+  }
+}
+let timeLeft = 60;
 //listeners
 $btn_play.addEventListener("click", startGame)
   
 
 
 //main functions
-async function startGame(e){
+function startGame(e) {
+  let actions = createGame() 
+  timePlay(actions)
+}
+
+function createGame() {
   clear()
-  const elem = e.target
-  elem.disabled = true
-  
-  while(timeLeft > 0){
-    ballKeep = await faceOff()
-    await new Promise(resolve => {
-      setTimeout(async function(){
-        if(ballKeep == "left"){
-          await throwBall(leftSideThrowChances)
-        }else{
-          await throwBall(rightSideThrowChances)
+  let result = []
+  while(result.length < 20) {
+    if (result.length == 0) {
+      result.push(faceOff(`Вбрасывание`, {startTime: 60}))
+    }
+    const prevAction = result[result.length - 1]
+    switch (prevAction.type) {
+      case "faceOff":
+        result.push(getBall(prevAction))
+        break;
+      case "getBall":
+        result.push(throwBall(prevAction))
+        break;
+      case "throwBall":
+        if (prevAction.throw) {
+          result.push(getBall(prevAction))
+        } else {
+          result.push(faceOff(`Подбор`, prevAction))
         }
-        resolve()
-        console.log(timeLeft--)
-      }, 0)
-    })
+        break;
+    }
   }
-  determWinner()
-  elem.disabled = false
+  console.log(result)
+  return result
 }
-async function throwBall(sideChances){
+createGame()
+function faceOff(message, {startTime}){
+  const playAction = {
+    side: getRandom(0, 100) > chances.faceOffChances ? "left" : "right" ,
+    message: showMessageAct.bind(null, message),
+    startTime: startTime - 1,
+    type: "faceOff"
+  }
+
+  return playAction
+}
+
+
+function getBall({side, startTime}) {
+  const playAction = {
+    side,
+    message: showMessageAct.bind(null,`Мяч у ${side}`),
+    startTime: startTime - getRandom(1, 2),
+    type: "getBall"
+  }
+
+  return playAction
+}
+
+
+// createGame()
+
+function timePlay(actions) {
+  let j = 0
+  let timer = setInterval(() => {
+    $timer.innerHTML = $timer.innerHTML - 1
+    console.log("timeLeft:", $timer.innerHTML)
+    if (actions[j].startTime == $timer.innerHTML) {
+      actions[j].message()
+      j++
+    }
+    if ($timer.innerHTML <= 0){
+      clearInterval(timer)
+    } 
+  }, 1000)
+}
+
+function throwBall({side, startTime}){
+  const playAction = {
+    type: "throwBall"
+  }
   const expectation = getRandom(0, 100)
-  if(expectation <= sideChances){
-    ballKeep == "left" ? leftSideScore += 2 : rightSideScore += 2
-    showMessageAct("Попал")
-    changeScore()
-    await new Promise(resolve => {
-      setTimeout(resolve, 1000);
-    })
-    timeLeft--
-    return
+  if (expectation < chances.throwChances[side]) {
+    playAction.message = function() {
+      showMessageAct(`${side} попал`)
+      plus2p(side)
+    }
+    playAction.throw = true
+    playAction.startTime = startTime - getRandom(1, 3)
+    playAction.side = side == "left" ? "right" : "left"
+  }else {
+    playAction.message = showMessageAct.bind(this, `${side} промахнулся`)
+    playAction.throw = false
+    playAction.startTime = startTime - getRandom(1, 4)
   }
-  showMessageAct("Промах")
-  await new Promise(resolve => {
-    setTimeout(resolve, 1000);
-  })
-  timeLeft--
+
+  return playAction
 }
+
+function plus2p(side) {
+  side == "left" ? leftSideScore += 2 : rightSideScore += 2
+  changeScore()
+}
+// async function startGame(e){
+//   timePlay()
+//   clear()
+//   const $elem = e.target
+//   $elem.disabled = true
+  
+//   while(timer.innerHTML > 0){
+//     if (!ballKeep) {
+//       await faceOff() 
+//       ballKeep = await getBall()
+//     }
+//     await new Promise(resolve => {
+//       setTimeout(async function(){
+//         if(ballKeep == "left"){
+//           await throwBall(leftSideThrowChances)
+//         }else{
+//           await throwBall(rightSideThrowChances)
+//         }
+//         resolve()
+//         console.log(timeLeft--)
+//       }, 0)
+//     })
+//   }
+//   determWinner()
+//   $elem.disabled = false
+// }
+
 
 function startTimer() {
   let timerId = setInterval(() => {
@@ -83,23 +180,9 @@ function determWinner(){
   }
 }
 
-async function faceOff(){
-  await new Promise(resolve => {
-    showMessageAct("Вбрасывание")
-    setTimeout(() => {
-      resolve()
-    }, 1000)
-  })
-  let side = await new Promise(resolve => {
-    let side = getRandom(0, 100) > leftSideChances ? "left" : "right"
-    showMessageAct("мяч у " + side)
-    setTimeout(() => {
-      resolve(side)
-    }, 1000 )
-  })
-  timeLeft--
-  return Promise.resolve(side)
-}
+
+
+
 
 function determSide(side){
   
@@ -125,7 +208,6 @@ function getRandom(min, max){
 function clear(){
   leftSideScore = 0;
   rightSideScore = 0;
-  timeLeft = 15;
 
   $messageWin.innerHTML = ""
   $score.innerHTML = "0:0"
